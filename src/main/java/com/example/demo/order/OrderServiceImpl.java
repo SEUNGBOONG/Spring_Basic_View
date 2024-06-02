cafb3de1c63b37f4e9bd912b4d1c8d194788099d
승엽 <tmdduqflfl@naver.com>
2024-06-02 17:21:25 +0900

    6/2 스프링 기초 공부

--- src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -15,6 +15,6 @@ public class OrderServiceImpl implements OrderService {
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-
+        return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
