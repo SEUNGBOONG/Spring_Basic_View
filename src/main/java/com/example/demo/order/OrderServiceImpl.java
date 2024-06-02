73e378fd02ab43ac894bc60266af66276978ca6c
승엽 <tmdduqflfl@naver.com>
2024-06-02 20:48:28 +0900

    Fix : - 인터페이스에만 의존하도록 OCP DIP 해결

--- src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -11,7 +11,9 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private DiscountPolicy discountPolicy; //인터페이스만 의존한다.
 
     @Override
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
